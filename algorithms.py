92c5f95a345fd0516232bb3160f0b46e56484034
Jaime Villarreal <jvillarreal337@gmail.com>
2021-05-27 19:58:49 -0500

    Added merge and quick sort, slight formatting.

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,13 @@
 """ Algorithms module """
 
-import random, time
+import random
+import time
 import visualizer
+
 
 class Algorithm:
     """ Base algorithms class """
+
     def __init__(self, name):
         # Randomized array.
         self.array = random.sample(range(512), 512)
@@ -22,8 +25,10 @@ class Algorithm:
         time_elapsed = time.time() - self.start_time
         return self.array, time_elapsed
 
+
 class SelectionSort(Algorithm):
     """ Selection sort implementation class """
+
     def __init__(self):
         super().__init__("SelectionSort")
 
@@ -36,8 +41,10 @@ class SelectionSort(Algorithm):
             self.array[i], self.array[min_idx] = self.array[min_idx], self.array[i]
             self.update_display(self.array[i], self.array[min_idx])
 
+
 class BubbleSort(Algorithm):
     """ Bubble sort implementation class """
+
     def __init__(self):
         super().__init__("BubbleSort")
 
@@ -47,3 +54,78 @@ class BubbleSort(Algorithm):
                 if self.array[j] > self.array[j+1]:
                     self.array[j], self.array[j+1] = self.array[j+1], self.array[j]
             self.update_display(self.array[j], self.array[j+1])
+
+class InsertionSort(Algorithm):
+    """ Insertion sort implementation class """
+
+    def __init__(self):
+        super().__init__("InsertionSort")
+
+    def algorithm(self):
+        for i in range(len(self.array)):
+            cursor = self.array[i]
+            idx = i
+            while idx > 0 and self.array[idx-1] > cursor:
+                self.array[idx] = self.array[idx-1]
+                idx -= 1
+            self.array[idx] = cursor
+            self.update_display(self.array[idx], self.array[i])
+
+class MergeSort(Algorithm):
+    """ Merge sort implementation class """
+
+    def __init__(self):
+        super().__init__("MergeSort")
+
+    def algorithm(self, array=[]):
+        if array == []:
+            array = self.array
+        if len(array) < 2:
+            return array
+        mid = len(array) // 2
+        left = self.algorithm(array[:mid])
+        right = self.algorithm(array[mid:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        result = []
+        i, j = 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+            self.update_display()
+        result += left[i:]
+        result += right[j:]
+        self.array = result
+        self.update_display()
+        return result
+
+class QuickSort(Algorithm):
+    """ Quick sort implementation class """
+
+    def __init__(self):
+        super().__init__("QuickSort")
+
+    def algorithm(self, array=[], start=0, end=0):
+        if array == []:
+            array = self.array
+            end = len(array) - 1
+        if start < end:
+            pivot = self.partition(array, start, end)
+            self.algorithm(array, start, pivot-1)
+            self.algorithm(array, pivot+1, end)
+        
+    def partition(self, array, start, end):
+        x = array[end]
+        i = start-1
+        for j in range(start, end+1, 1):
+            if array[j] <= x:
+                i += 1
+                if i < j:
+                    array[i], array[j] = array[j], array[i]
+                    self.update_display(array[i], array[j])
+        return i
